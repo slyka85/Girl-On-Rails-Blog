a785e4521cb7fc6d9d4b879831e833c38c4efadf
Anya Melnyk <slyka85@gmail.com>
2015-10-29 14:04:19 -0400

    redcarpet and coderay added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,7 @@ class PostsController < ApplicationController
 	end
 
 	def show
+		
 	end
 
 	def edit
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,49 @@
 module ApplicationHelper
+
+	class CodeRayify < Redcarpet::Render::HTML
+	  def block_code(code, language)
+	    CodeRay.scan(code, language).div
+	       # CodeRay.scan(code, language).div(:line_numbers => :table)
+	  end
+	end
+
+	def markdown(text)
+	  coderayified = CodeRayify.new(:filter_html => true, 
+	                                :hard_wrap => true)
+	  options = {
+	    :fenced_code_blocks => true,
+	    :no_intra_emphasis => true,
+	    :autolink => true,
+	    :strikethrough => true,
+	    :lax_html_blocks => true,
+	    :superscript => true
+	  }
+	  markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+	  markdown_to_html.render(text).html_safe
+	end
 end
+
+
+# class CodeRayify < Redcarpet::Render::HTML
+#     def block_code(code, language)
+#         CodeRay.scan(code, language).div(:line_numbers => :inline)
+#     end
+# end
+
+# def markdown(text)
+#     coderayified = CodeRayify.new(:filter_html => true, :hard_wrap => true)
+
+#     language ||= :plaintext
+
+#     options = {
+#         :fenced_code_blocks => true,
+#         :no_intra_emphasis => false,
+#         :autolink => true,
+#         :strikethrough => true,
+#         :lax_html_blocks => true,
+#         :superscript => true
+#     }
+
+#     markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+#     markdown_to_html.render(text).html_safe
+#     end
